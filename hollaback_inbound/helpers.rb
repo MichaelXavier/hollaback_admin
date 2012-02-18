c5b42f048ed17b72818490db45924c3564f64653
Michael Xavier <michael@michaelxavier.net>
2012-02-17 21:51:54 -0800

    Spec inbound helpers

--- hollaback_inbound/helpers.rb
+++ hollaback_inbound/helpers.rb
@@ -1,7 +1,11 @@
+require 'active_support/core_ext/numeric/time'
+require 'active_support/json'
+
 module HollabackInbound
   module Helpers
     def simplify_payload(json)
-      offset = calculate_offset(json['Headers']['Date'])
+      date = json.fetch('Headers').detect {|h| h['Name'] == 'Date'}.fetch('Value')
+      offset = calculate_offset(date)
       {
         'from'           => json["From"],
         'to'             => json["To"],
@@ -14,11 +18,12 @@ module HollabackInbound
     # Calculate offset in seconds from full date string
     # e.g.: Fri, 17 Feb 2012 20:19:46 -0800
     def calculate_offset(date_str)
-      #TODO  
+      hours, mins, secs = DateTime.parse(date_str).strftime("%::z").split(':')
+      hours.to_i.hours + mins.to_i.minutes + secs.to_i
     end
 
     def store(payload)
-      redis.lpush('messages', simplify_payload(payload).to_json)
+      redis.rpush('messages', simplify_payload(payload).to_json)
     end
   end
 end
--- spec/hollaback_inbound/helpers_spec.rb
+++ spec/hollaback_inbound/helpers_spec.rb
@@ -8,7 +8,63 @@ module HollabackInbound
 
     describe "#calculate_offset" do
       it "returns 0 for GMT" do
-        subject.calculate_offset("Fri, 17 Feb 2012 20:19:46 -0800").should == 0
+        subject.calculate_offset("Fri, 17 Feb 2012 20:19:46 -0000").
+          should == 0
+      end
+
+      it "returns the proper time for pacific time" do
+        subject.calculate_offset("Fri, 17 Feb 2012 20:19:46 -0800").
+          should == -28800
+      end
+    end
+
+    describe "payload handling" do
+      let(:payload) do
+        {
+          "Headers" => [{
+            "Name"  => "Date",
+            "Value" => "Fri, 17 Feb 2012 20:19:46 -0800"
+          }],
+          "From"     => "joe@example.com",
+          "To"       => "3h@hollaback.example.com",
+          "Subject"  => "holla back!",
+          "TextBody" => "woop woop"
+        }
+      end
+
+      describe "#simplify_payload" do
+        it "parses out the relevant data" do
+          subject.simplify_payload(payload).should == {
+            'from'           => "joe@example.com",
+            'to'             => "3h@hollaback.example.com",
+            'subject'        => "holla back!",
+            'body'           => "woop woop",
+            'offset_seconds' => -28800
+          }
+        end
+      end
+
+      describe "#store" do
+        let(:redis) { mock("Redis", :rpush => 1) }
+
+        before(:each) do
+          subject.stub(:redis).and_return(redis)
+        end
+
+        it "pushes the simplified payload to the end of the message queue" do
+          redis.should_receive(:rpush).with do |key, json|
+            key == "messages" &&
+            JSON.parse(json) == {
+              'from'           => "joe@example.com",
+              'to'             => "3h@hollaback.example.com",
+              'subject'        => "holla back!",
+              'body'           => "woop woop",
+              'offset_seconds' => -28800
+            }
+          end
+
+          subject.store(payload)
+        end
       end
     end
   end
