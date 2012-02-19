daddc7e144f3283def58e02da0086892d76039ac
Michael Xavier <michael@michaelxavier.net>
2012-02-19 11:12:43 -0800

    Fix MessageSchedule#soonest

--- lib/message_schedule.rb
+++ lib/message_schedule.rb
@@ -1,10 +1,8 @@
+require 'lib/scheduled_message'
+
 class MessageSchedule
   def initialize(redis)
     @redis = redis
-  end
-
-  def empty?
-    false #TODO
   end
 
   def soonest(count)
@@ -14,18 +12,23 @@ class MessageSchedule
 private
 
   def take_messages(tstamps, count)
+    return [] if count == 0
+    tstamps   = tstamps.to_a
     remaining = count
-    [].tap do |messages|
-      while ts = tstamps.shift && remaining > 0
-        payloads_for_ts = redis.lrange(ts, 0, remaining)
-        messages += payloads_for_ts.map {|pl| ScheduledMessage.new(pl, ts)}
-        remaining -= payloads_for_ts.length
-      end
+    messages = []
+
+    while remaining > 0 && ts = tstamps.shift
+      payloads_for_ts = redis.lrange(ts, 0, remaining - 1)
+      messages += payloads_for_ts.map {|pl| ScheduledMessage.new(pl, ts)}
+      remaining -= payloads_for_ts.length
     end
+
+    messages
   end
 
   def timestamps(count)
-    redis.zrange("schedule", 0, count).to_set
+    return [] if count == 0
+    redis.zrange("schedule", 0, count - 1).to_set
   end
 
   def parse_timestamp(str)
--- spec/message_schedule_spec.rb
+++ spec/message_schedule_spec.rb
@@ -1,10 +1,42 @@
+require 'redis-namespace'
 require 'lib/message_schedule'
 
 describe MessageSchedule do
   let(:redis) { Redis::Namespace.new("hollaback_test", :redis => Redis.new) }
+  let(:timestamps) { %w[1329636687 1329636686 1329636688]}
+  let(:payload) { '{"Subject":"holla back"}'}
 
   subject {MessageSchedule.new(redis) }
-  describe "defaults" do
-  
+
+  before(:each) do
+    redis.del(*timestamps)
+    timestamps.each do |ts|
+      redis.zadd('schedule', ts.to_i, ts)
+      redis.lpush(ts, payload)
+    end
+  end
+
+  after(:each) do
+    redis.del('schedule')
+    redis.del(*timestamps)
+  end
+
+  describe "#soonest" do
+    context "count of 0" do
+      it "returns an empty array" do
+        subject.soonest(0).should == []
+      end
+    end
+
+    context "count > 0" do
+      it "draws from the schedule and timestamp stores, timestamp asc order" do
+        messages = subject.soonest(2)
+        messages.length.should == 2
+        messages[0].deliver_at.to_i.should == 1329636686
+        messages[0].subject.should == "holla back"
+        messages[1].deliver_at.to_i.should == 1329636687
+        messages[1].subject.should == "holla back"
+      end
+    end
   end
 end
