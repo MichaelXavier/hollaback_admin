2252d5c20ff0b1e915ff22304fa412006f5a8327
Michael Xavier <michael@michaelxavier.net>
2012-04-23 18:21:13 -0700

    prepent to require path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
 require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+$:.unshift(File.dirname(File.join(__FILE__, '..')))
 
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
